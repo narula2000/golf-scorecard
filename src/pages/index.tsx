fd4977ef8931429840f17fe3d8bb4ae102f291d3
Vikrom Narula <vikrom.nar@gmail.com>
2023-03-30 14:41:06 +0700

    Change the href to correct pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
+              href="/dashboard"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Dashboard</h3>
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              href="/front-9"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Front 9</h3>
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              href="/back-9"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">Back 9</h3>
@@ -48,7 +48,7 @@ const Home: NextPage = () => {
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
+              href="/about-us"
               target="_blank"
             >
               <h3 className="text-2xl font-bold">About us</h3>
