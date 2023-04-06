7d8f877863d754672c52c7c44644041f8e863304
Vikrom Narula <vikrom.nar@gmail.com>
2023-04-06 08:36:11 +0700

    Update prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,12 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss"), require.resolve("@trivago/prettier-plugin-sort-imports")],
-  importOrder: ["^components/(.*)$", "^[./]"],
+  printWidth: 80,
+  tabWidth: 2,
+  trailingComma: "all",
+  singleQuote: true,
+  semi: true,
+  importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true
 };
